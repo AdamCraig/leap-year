86aa0cd7d813c77d34bfbd33e6a8dc44ec0fae09
Adam Craig and Joshua Logan <adam@>
2016-03-30 08:32:56 -0700

    added user interface and user interface logic in js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
